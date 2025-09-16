eddbc57b4b5cb1862cf9cdc9a23a13d06cd8d05f
Lucas Argerich <lucasargerichcoder@gmail.com>
2025-09-16 08:44:05 -0300

    feat: implement event handling for flight creation, updates, and deletion

--- back/src/routes/events.ts
+++ back/src/routes/events.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
+import eventController from '../controllers/events'
 
 const router = Router()
 
-// Ruta de ejemplo para obtener vuelos
-router.post('/', (req, res) => {
-  res.send('Post events route')
-})
+router.post('/', eventController.handleEvent)
 
 export default router
