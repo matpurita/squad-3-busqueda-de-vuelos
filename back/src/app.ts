7e78c9b0ea24ead88ad02ae8ea2f6d58dc183f26
Lucas Argerich <lucasargerichcoder@gmail.com>
2025-10-20 10:30:57 -0300

    refactor: replace authMiddleware with requireAuth for improved authorization handling

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,23 +2,23 @@ import express from 'express'
 import searchRouter from './routes/search'
 import airportRouter from './routes/airport'
 import authRouter from './routes/auth'
-import metricsRouter from './routes/metrics'
 import { errorHandler } from './middlewares/error'
 import cors from 'cors'
 import { connectConsumer } from './kafka/kafka'
+import { authMiddleware } from './middlewares/auth'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use(authMiddleware)
+
 app.use('/auth', authRouter)
 
 app.use('/search', searchRouter)
 
 app.use('/airport', airportRouter)
-
-app.use('/metrics', metricsRouter)
 
 app.use(errorHandler)
 
--- back/src/routes/auth.ts
+++ back/src/routes/auth.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import authController from '../controllers/auth'
 import authMock from '../controllers/authMock'
-import { authMiddleware } from '../middlewares/auth'
+import { requireAuth } from '../middlewares/auth'
 
 const router = Router()
 
 router.post('/login', authController.login)
 router.post('/login/mock', authMock.loginMock) // Ruta mock independiente
-router.get('/user', authMiddleware, authController.getUserData)
+router.get('/user', requireAuth, authController.getUserData)
 
 export default router
