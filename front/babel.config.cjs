3288c9bccd4b36a27d1f9abddec268c65aec8c74
Lucas Argerich <lucasargerichcoder@gmail.com>
2025-11-13 19:19:39 -0300

    Fix root package scripts

--- front/babel.config.cjs
+++ front/babel.config.cjs
@@ -1,7 +1,7 @@
-export default {
+module.exports = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
-     ['@babel/preset-react', {runtime: 'automatic'}],
-  ],
+     ['@babel/preset-react', {runtime: 'automatic'}]
+  ]
 };
